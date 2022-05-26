926a313bfc2c4d94ce63fdf330324f2cfd0b4953
aifamsmax <aifamsmax@gmail.com>
2022-05-26 18:43:18 +0530

    updated script.js, fetching multiple songs

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
-		'X-RapidAPI-Key': 'c109968898msh779b1aadd0374acp10f942jsn1368de5620cb'
+		'X-RapidAPI-Key': 'bd6a5c035dmsh03229b43c404b35p1bc1abjsnc6f328f24133'
 	}
 };
 
@@ -21,19 +21,25 @@ async function getData(song){
 
 function showData(song){
     console.log(song);
-    const title = song.data[0].title_short;
-    const artist = song.data[0].artist.name;
-    const img = song.data[0].album.cover_medium;
-    const albumTitle = song.data[0].album.title;
+    console.log(song.data[0]);
+    main.innerHTML='';
+   for(let i=0;i<10;i++) 
+   {
+    const title = song.data[i].title_short;
+    const artist = song.data[i].artist.name;
+    const img = song.data[i].album.cover_medium;
+    const albumtitle = song.data[i].album.title;
+    let x = albumtitle.length > 23 ? albumtitle.slice(0,23)+"..." : albumtitle;
 
-    str = `
-        <div class="card" style="width: 18rem; margin-right: 20px; position:relative;">
+    const el= document.createElement("div");
+    const str = `
+        <div class="card" style="width: 18rem; margin: 20px; position:relative;">
             <img src="${img}" class="card-img-top" alt="${title}">
             <div class="card-body">
                 <p class="card-text">
                 <b>Title:</b> ${title} <br>
                 <b>Artist:</b> ${artist} <br>
-                <b>Album:</b> ${albumTitle} <br>
+                <b>Album:</b> ${x} <br>
                 </p>
             </div>
             <div id="play">
@@ -41,10 +47,35 @@ function showData(song){
             </div>            
         </div>
     `
-    main.innerHTML = str;
+    el.innerHTML=str;
+    main.appendChild(el);
 }
+}
+// const title = song.data[0].title_short;
+// const artist = song.data[0].artist.name;
+// const img = song.data[0].album.cover_medium;
+// const albumTitle = song.data[0].album.title;
+
+// str = `
+//     <div class="card" style="width: 18rem; margin-right: 20px; position:relative;">
+//         <img src="${img}" class="card-img-top" alt="${title}">
+//         <div class="card-body">
+//             <p class="card-text">
+//             <b>Title:</b> ${title} <br>
+//             <b>Artist:</b> ${artist} <br>
+//             <b>Album:</b> ${albumTitle} <br>
+//             </p>
+//         </div>
+//         <div id="play">
+//           <a href="https://open.spotify.com/search/${title}" class="btn btn-outline-primary" role="button" target ="_blank">Click to Play !</a>
+//         </div>            
+//     </div>
+// `
+// main.innerHTML = str;
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     getData(search.value);
 })
+
+getData("top 50")
